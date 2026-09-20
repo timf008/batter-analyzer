129a36034de0be4166be7219cf386e777c7598e6
Tim <93391156+timf008@users.noreply.github.com>
2026-09-19 21:35:40 -0400

    Change TEST_FREE_MODE to true for testing

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ loadBatterOfDay(season);
 // true  = simulate Free Trial
 // false = simulate All Access
 
-const TEST_FREE_MODE = false;
+const TEST_FREE_MODE = true;
 
 
 // ------------------------------
